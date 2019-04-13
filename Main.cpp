e8cfd8368084f4ade1a375bd201d011b16178239
Zalek <ZalekDev@gmail.com>
2019-04-13 17:17:06 -0500

    Pre-refactor

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,8 @@
+//#define WIN32_LEAN_AND_MEAN
+//#include <windows.h>
+//#include <SDKDDKVer.h>
 #include <time.h>
-#include "stdafx.h"
+
 //#include "Console.h"
 #include "Engine.h"
 #include "Hooks.h"
